3c9368c488ec41cf85273d6669029c2e22bef1df
Alisson de Andrade Araújo <39311340+alissonandrade2020@users.noreply.github.com>
2021-03-12 08:23:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ app.use(express.json());
 
 const repositories = [];
 
+app.get("/", (request, response) => {
+    const query = request.query;
+    return response.json([
+        " ------- API -------",
+        "Nome: Alisson de Andrade Araújo",
+        "Formação: Analise e Desenvolvimento de Sistemas",
+        "Descrição: Desenvolvedor Back-end, Front-end e Mobile",
+        "Site: http://alissondeandradearaujo.000webhostapp.com/",
+        "CV: https://alissonandradesistema.000webhostapp.com/curriculo/",
+        "Currículo Lattes: http://lattes.cnpq.br/7594653859194302/",
+        "Github: https://github.com/alissonandrade2020", 
+        "Rocketseat: https://app.rocketseat.com.br/me/alissondeandradearaujo", 
+        "Linkedin: https://www.linkedin.com/in/alisson-de-andrade-ara%C3%BAjo-160224190/"])
+});
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
